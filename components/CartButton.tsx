a51d46eec170aa681f5dc81fb994b1e4dc1023cc
Licht Lumie <lichtlumie@Lichts-MacBook-Air.local>
2025-07-15 12:57:09 +0800

    trying to overcome tardiness by coding this

---
+++ components/CartButton.tsx
@@ -0,0 +1,18 @@
+import {Image, View, Text, TouchableOpacity} from 'react-native'
+import {images} from '@/constants'
+import React from 'react'
+
+const CartButton = () => {
+    const totalItems = 10;
+    return (
+        <TouchableOpacity className={"cart-btn"} onPress={() => {}}>
+            <Image source={images.bag} className={"size-5"} resizeMode={"contain"}/>
+            {totalItems > 0 && (
+                <View className={"cart-badge"}>
+                <Text className={"small-bold text-white"}>{totalItems}</Text>
+                </View>
+            )}
+        </TouchableOpacity>
+    )
+}
+export default CartButton
